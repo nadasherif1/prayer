02b0d8dc22b71b06b03736b69dbe2d2c2700c008
nadasherif1 <nsns01223@gmail.com>
2024-11-23 17:41:24 +0200

    updates in hadith

--- src/app/hadith/page.js
+++ src/app/hadith/page.js
@@ -1,68 +1,209 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
+const hisnMuslimHadiths = [
+  {
+    arab: "بِسْمِ اللَّهِ، تَوَكَّلْتُ عَلَى اللَّهِ، وَلَا حَوْلَ وَلَا قُوَّةَ إِلَّا بِاللَّهِ.",
+    reference: "الذكر عند الخروج من المنزل",
+  },
+  {
+    arab: "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنْ زَوَالِ نِعْمَتِكَ، وَتَحَوُّلِ عَافِيَتِكَ.",
+    reference: "الدعاء عند الكرب",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنْ التَّوَّابِينَ، وَاجْعَلْنِي مِنَ الْمُتَطَطَّهِرِينَ.",
+    reference: "الدعاء بعد الوضوء",
+  },
+  {
+    arab: "رَبِّ اغْفِرْ لِي وَارْحَمْنِي وَاهْدِنِي وَاجْبُرْنِي وَعَافِنِي وَارْزُقْنِي.",
+    reference: "الدعاء بين السجدتين",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْ قُبُورَنَا رَوْضَةً مِنْ رِيَاضِ الْجَنَّةِ.",
+    reference: "الدعاء عند زيارة القبور",
+  },
+  {
+    arab: "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنْ فَتْنَةِ الْمَسِيحِ الدَّجَّالِ.",
+    reference: "الدعاء من فتنة الدجال",
+  },
+  {
+    arab: "اللَّهُمَّ حَبِّبْ إِلَيَّ الْإِيمَانَ وَزَيِّنْهُ فِي قَلْبِي.",
+    reference: "الدعاء بحب الإيمان",
+  },
+  {
+    arab: "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنَ الْمُتَعَجِّلِ وَالْمُتَأَخِّرِ.",
+    reference: "الدعاء عند الاستعاذة بالله",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنْ رَحْمَتِكَ وَمِنْ غَيْرِ مَشَقَّةٍ فِي دِينِي.",
+    reference: "الدعاء للمغفرة",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي فِي رُفْقٍ وَعَفْوٍ فِي دِينِي.",
+    reference: "الدعاء لرفع البلاء",
+  },
+  {
+    arab: "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنَ الشِّرْكِ وَالشِّرْكِ الدَّجَّالِ.",
+    reference: "الدعاء من الشرك والدجال",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنْ عِبَادِكَ الْمُؤْمِنِينَ وَقِيِّمِينَ لِأَمْرِكَ.",
+    reference: "الدعاء للهداية والإيمان",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنْ أَسْبَابِ نَجَاتِكَ وَمِنْ فَاتِحَةِ رَحْمَتِكَ.",
+    reference: "الدعاء للنجاة من النار",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنْ رَحْمَتِكَ الْوَاسِعَةِ.",
+    reference: "الدعاء لرحمة الله",
+  },
+  {
+    arab: "اللَّهُمَّ لَا تَجْعَلْنِي فِي فَتْنَةٍ تَقُودُ إِلَى حَزْنٍ وَجَزَعٍ.",
+    reference: "الدعاء لعدم الوقوع في الفتن",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي فِي رَحْمَتِكَ وَمِنْ سَبَبٍ لِجَنَّتِكَ.",
+    reference: "الدعاء للوصول إلى الجنة",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنْ أَهْلِ جَنَّتِكَ الَّذِينَ لَا خَوْفٌ عَلَيْهِمْ وَلَا هُمْ يَحْزَنُونَ.",
+    reference: "الدعاء للنجاة من النار",
+  },
+  {
+    arab: "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنَ الْفَقْرِ وَالْمُسْكَنَةِ وَالذُّلِّ وَالْجُوعِ.",
+    reference: "الدعاء ضد الفقر",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِمَّنْ تَقَبَّلَ صَلَاتِي وَأَجْبَرَ عَثْرَتِي.",
+    reference: "الدعاء لقبول الصلاة",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنَ الَّذِينَ يُسْرِفُونَ فِي طَاعَتِكَ وَلَا تَجْعَلْنِي مِنَ الْمُقَصِّرِينَ.",
+    reference: "الدعاء للسعي في الطاعة",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنَ الَّذِينَ يَشْفَعُونَ فِي الْمُؤْمِنِينَ يَوْمَ الْقِيَامَةِ.",
+    reference: "الدعاء للمغفرة",
+  },
+  {
+    arab: "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنْ غَضَبِكَ وَمِنْ سَخَطِكَ.",
+    reference: "الدعاء للسلامة من غضب الله",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنَ الشَّاكِرِينَ لِفَضْلِكَ وَمِنَ السَّابِقِينَ فِي عِبَادَتِكَ.",
+    reference: "الدعاء للشكر والعبادة",
+  },
+  {
+    arab: "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنْ حُبِّ الدُّنْيَا وَزِينَتِهَا.",
+    reference: "الدعاء من حب الدنيا",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي فِي رَحْمَتِكَ الَّتِي تَحْتَوِي كُلَّ شَيْءٍ.",
+    reference: "الدعاء لرحمة الله الواسعة",
+  },
+  {
+    arab: "اللَّهُمَّ اجْعَلْنِي مِنَ الَّذِينَ يُؤْثِرُونَ وَيُحِبُّونَ جَمِيعَ عِبَادِكَ.",
+    reference: "الدعاء للوحدة والمودة بين المسلمين",
+  },
+];
+
 
 const Hadith = () => {
-  const [hadith, setHadith] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [categories] = useState([
+    { id: "bukhari", name: "صحيح البخاري" },
+    { id: "muslim", name: "صحيح مسلم" },
+    { id: "hisnMuslimHadiths", name: "حصن المسلم "},
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [hadiths, setHadiths] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchHadiths = async () => {
-      try {
+  const fetchHadiths = async (categoryId) => {
+    setLoading(true);
+    setHadiths([]);
+
+    try {
+      if (categoryId === "hisnMuslimHadiths") {
+        setHadiths(hisnMuslimHadiths);
+      } else {
         const response = await fetch(
-          "https://hadithapi.com/public/api/hadiths?apiKey=$2y$10$EhOBQubaytO2zayidmAzOPfFReUtsaKZPpGdubuMGxYywzTllme"
+          `https://api.hadith.gading.dev/books/${categoryId}?range=1-300`
         );
 
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("Failed to fetch hadiths");
         }
 
         const data = await response.json();
-        console.log(data);
 
-        if (data.hadiths && data.hadiths.data) {
-          setHadith(data.hadiths.data);
-        } else {
-          console.error("No hadiths found in the response");
-        }
-      } catch (error) {
-        console.error("Error fetching hadiths:", error);
-      } finally {
-        setLoading(false);
+        const filteredHadiths = data.data.hadiths.filter(
+          (hadith) => hadith.arab.length < 500
+        );
+
+        setHadiths(filteredHadiths);
       }
-    };
+    } catch (error) {
+      console.error("Error fetching hadiths:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    fetchHadiths();
-  }, []);
+  const handleCategoryClick = (categoryId) => {
+    setSelectedCategory(categoryId);
+    fetchHadiths(categoryId);
+  };
 
   return (
-    <div className="mt-[150px] px-4 sm:px-6 lg:px-8 ">
-      <h1 className="text-2xl sm:text-3xl font-bold pt-6 p-5 text-center">
+    <div className="mt-[80px] px-4 sm:px-6 lg:px-8 " dir="rtl">
+      <h1 className="text-2xl sm:text-3xl font-bold pt-6 pb-9 text-center">
         الأحاديث النبوية
       </h1>
-      {loading ? (
-        <p className="text-base sm:text-lg font-bold text-center">
-          جاري تحميل الأحاديث...
-        </p>
-      ) : (
-        <div className="flex flex-col gap-6 items-center mb-[30px] lg:items-stretch lg:w-[80%] lg:mx-auto">
-          {hadith.map((item, index) => (
+
+      <div className="flex flex-wrap justify-center gap-4 mb-10">
+        {categories.map((category) => (
+          <button
+            key={category.id}
+            onClick={() => handleCategoryClick(category.id)}
+            className={`px-4 py-2 rounded-lg border ${
+              selectedCategory === category.id
+                ? "bg-gray-600 text-white"
+                : "bg-gray-100 text-gray-800"
+            } hover:bg-gray-500 hover:text-white transition`}
+          >
+            {category.name}
+          </button>
+        ))}
+      </div>
+
+      {loading && <p className="text-center text-gray-500">جاري تحميل الأحاديث...</p>}
+
+      {!loading && hadiths.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-5">
+          {hadiths.map((hadith, index) => (
             <div
               key={index}
-              className="p-4 rounded-lg shadow-md border border-gray-200 transition-transform duration-200 hover:scale-105 w-full"
+              className="p-4 rounded-lg shadow-md border border-gray-200 transition-transform duration-200 hover:scale-105"
             >
               <p className="text-lg sm:text-xl font-bold text-center">
-                {item.hadithArabic}
+                {hadith.arab} 
               </p>
-              <p className="text-sm text-gray-500 mt-2 text-center">
-                المصدر: {item.book.bookName}
-              </p>
-              <p className="text-sm text-gray-500 mt-2 text-center">
-                الراوي: {item.englishNarrator}
-              </p>
+              {hadith.reference && (
+                <p className="text-sm text-gray-500 mt-2 text-center">
+                  المرجع: {hadith.reference}
+                </p>
+              )}
+             
             </div>
           ))}
         </div>
+      ) : (
+        !loading &&
+        selectedCategory && (
+          <p className="text-center text-gray-500">لا توجد أحاديث لعرضها.</p>
+        )
       )}
     </div>
   );
